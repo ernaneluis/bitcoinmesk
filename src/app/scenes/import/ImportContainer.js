dc86f290da9f70403e094b054d78942a78886654
Ernane Luis <ernane.luis@gmail.com>
2018-06-28 17:47:12 +0900

    add import container file

---
+++ src/app/scenes/import/ImportContainer.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux'
+
+import Import from './Import'
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => ({})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Import)
