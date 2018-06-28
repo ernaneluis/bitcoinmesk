9ade11fdc150ef5a453f919c4808e9604b16bd9e
Ernane Luis <ernane.luis@gmail.com>
2018-06-28 17:13:26 +0900

    add header container file

---
+++ src/app/components/header/HeaderContainer.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux'
+import { toWallet, toCreate, toImport } from '../../routerActions'
+import Header from './Header'
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => ({
+  onToWallet: () => dispatch(toWallet()),
+  onToCreate: () => dispatch(toCreate()),
+  onToImport: () => dispatch(toImport()),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header)
