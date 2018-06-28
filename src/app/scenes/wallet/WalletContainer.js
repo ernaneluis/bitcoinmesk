270309b77715eefaa589ef9d07e4195c62c6f9cb
Ernane Luis <ernane.luis@gmail.com>
2018-06-28 17:47:40 +0900

    add wallet component file

---
+++ src/app/scenes/wallet/WalletContainer.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux'
+
+import Wallet from './Wallet'
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => ({})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Wallet)
