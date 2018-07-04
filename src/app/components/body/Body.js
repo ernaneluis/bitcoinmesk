c5d262d5c46e78a979b1702b20acfa31eab829b7
Ernane Luis <ernane.luis@gmail.com>
2018-07-05 03:33:00 +0900

    add init scene to body

--- src/app/components/body/Body.js
+++ src/app/components/body/Body.js
@@ -2,13 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Wallet from '../../scenes/wallet/WalletContainer'
+import Init from '../../scenes/init/InitContainer'
 import Create from '../../scenes/create/CreateContainer'
 import Import from '../../scenes/import/ImportContainer'
 
 const Container = styled.main.attrs({
   className: 'container',
 })`
-  padding: 60px 15px 0;
+  min-height: 500px;
+  &&& {
+    padding: 80px 15px 0;
+  }
+  @media (max-width: ${props => props.theme.screenSize.md}) {
+    &&& {
+      padding: 200px 15px 0;
+    }
+  }
   @media (max-width: ${props => props.theme.screenSize.lg}) {
     &.container {
       width: 100%;
@@ -20,6 +29,7 @@ const Container = styled.main.attrs({
 const Body = ({ scene }) => (
   <Container>
     {scene === 'WALLET' && <Wallet />}
+    {scene === 'INIT' && <Init />}
     {scene === 'CREATE' && <Create />}
     {scene === 'IMPORT' && <Import />}
   </Container>
