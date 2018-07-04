b5674f33e40bc571c15ccc559a8481fb22d35eae
Ernane Luis <ernane.luis@gmail.com>
2018-07-05 03:24:44 +0900

    move router action to store folder

--- src/store/actions/routerActions.js
+++ src/store/actions/routerActions.js
@@ -1,3 +1,4 @@
 export const toWallet = () => ({ type: 'WALLET' })
 export const toCreate = () => ({ type: 'CREATE' })
 export const toImport = () => ({ type: 'IMPORT' })
+export const toInit = () => ({ type: 'INIT' })
