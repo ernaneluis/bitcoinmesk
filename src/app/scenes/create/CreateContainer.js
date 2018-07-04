a04b763b059559b845baa3179b73605d220c7b25
Ernane Luis <ernane.luis@gmail.com>
2018-07-05 03:30:19 +0900

    add create address container with redux form connected

--- src/app/scenes/create/CreateContainer.js
+++ src/app/scenes/create/CreateContainer.js
@@ -1,12 +1,28 @@
 import { connect } from 'react-redux'
-
+import { reduxForm } from 'redux-form'
 import Create from './Create'
+import { createAddress } from '../../../store/actions/createActions'
 
 const mapStateToProps = state => ({})
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+  dispatch,
+})
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Create)
+const mergeProps = (stateProps, { dispatch }, { handleSubmit }) => ({
+  ...stateProps,
+  dispatch,
+  onSubmit: handleSubmit(values => dispatch(createAddress(values))),
+})
+
+export default reduxForm({
+  form: 'createForm',
+  destroyOnUnmount: false,
+  forceUnregisterOnUnmount: true,
+})(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+    mergeProps
+  )(Create)
+)
