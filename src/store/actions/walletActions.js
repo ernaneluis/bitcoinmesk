01c4f21f5ab343790adc6fc5a60df8a900d303c7
Ernane Luis <ernane.luis@gmail.com>
2018-07-05 14:02:29 +0900

    refactor wallet actions by supporting new reducer types and private key action function

--- src/store/actions/walletActions.js
+++ src/store/actions/walletActions.js
@@ -1,7 +1,18 @@
 import {
-  CREATE_ADDRESS_FAILURE,
-  CREATE_ADDRESS_REQUEST,
-  CREATE_ADDRESS_SUCCESS,
-} from '../../../../typesReducers.js'
+  CREATE_PRIVATE_KEY_FAILURE,
+  CREATE_PRIVATE_KEY_REQUEST,
+  CREATE_PRIVATE_KEY_SUCCESS,
+} from '../typesReducers'
 
-import * as bitcoin from '../../../lib/bitcoin'
+import * as bitcoin from '../../app/lib/bitcoin'
+
+export const createPrivateKeyFromMnemonic = mnemonic => dispatch => {
+  dispatch({ type: CREATE_PRIVATE_KEY_REQUEST })
+  return bitcoin.mnemonic
+    .createHDPrivateFromMnemonic(mnemonic)
+    .then(hdPrivate => bitcoin.private.createPrivateKeyFromHDPrivate(hdPrivate))
+    .then(privateKey =>
+      dispatch({ type: CREATE_PRIVATE_KEY_SUCCESS, payload: privateKey })
+    )
+    .catch(error => dispatch({ type: CREATE_PRIVATE_KEY_FAILURE, error }))
+}
