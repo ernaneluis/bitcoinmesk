71a266defbbd6070c62bf54b6f43174d13a2135f
Ernane Luis <ernane.luis@gmail.com>
2018-07-06 02:23:25 +0900

    refactor mnemonic by creating 128 bits entropy from seed hex

--- src/app/lib/bitcoin/bitcoin.mnemonic.js
+++ src/app/lib/bitcoin/bitcoin.mnemonic.js
@@ -1,11 +1,19 @@
 import * as Mnemonic from 'bitcore-mnemonic'
-
+import utils from './bitcoin.utils'
+// entropy > seed > mnemonic + password > hdkey
 const createMnemonicFromSeed = seedHex =>
   new Promise((resolve, reject) => {
     try {
-      let seed = new Buffer(seedHex, 'hex')
-      seed = seed.slice(0, 32)
-      // suppose to reproduce 24 words mnemonic
+      // convert seex hex to bytes array
+      const seedHexBytes = seedHex.match(/.{1,2}/g).map(b => parseInt(b, 16))
+      let selectedBytes = new Uint8Array(16)
+      // populate selectedBytes with random selected elements from seedHexBytes
+      selectedBytes = selectedBytes.map(e =>
+        utils.getRandomInt(0, seedHexBytes.length)
+      )
+      // reduce the hex to bytes array  to 16bytes lenght
+      const seed = Buffer.from(selectedBytes)
+      // 128 bits = 16 bytes will generate 12 words mnemonic
       const mnemonic = Mnemonic.fromSeed(seed, Mnemonic.Words.ENGLISH)
       resolve(mnemonic)
     } catch (error) {
