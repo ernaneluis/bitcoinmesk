1e710556b63d13b458c1096cdfd3eb7df68ffb50
Ernane Luis <ernane.luis@gmail.com>
2018-07-06 02:22:16 +0900

    refactor seeder to support new name secure random file

--- src/app/lib/bitcoin/bitcoin.seeder.js
+++ src/app/lib/bitcoin/bitcoin.seeder.js
@@ -1,4 +1,4 @@
-import * as crypto from './crypto'
+import * as secureRandom from './secureRandom'
 import utils from './bitcoin.utils'
 
 class Seeder {
@@ -6,17 +6,17 @@ class Seeder {
   constructor(seedLimit) {
     this.seedLimit = seedLimit || utils.getRandomInt(100, 200)
     this.seedCount = 0
-    this.lastInputTime = 0
+    this.lastInputTime = new Date().getTime()
     this.seed = 0
 
     this.updateSeedWithEntropy(this.seedLimit)
   }
 
   updateSeedWithEntropy = entropy => {
-    crypto.SecureRandom.seedTime()
+    secureRandom.SecureRandom.seedTime()
     // add more entropy to the pool
-    crypto.SecureRandom.seedInt16(entropy)
-    this.seed = utils.bytesToHex(crypto.SecureRandom.pool)
+    secureRandom.SecureRandom.seedInt16(entropy)
+    this.seed = utils.bytesToHex(secureRandom.SecureRandom.pool)
   }
 
   addEntropyToSeed = (entropy, timeStamp) => {
