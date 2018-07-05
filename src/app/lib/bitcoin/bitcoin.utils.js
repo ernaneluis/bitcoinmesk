88f1857b0ea4535053062fd7a036502f60cd2a21
Ernane Luis <ernane.luis@gmail.com>
2018-07-06 02:22:42 +0900

    refactor bitcoin utils by adding encrypt and decrypt functions from crypto-js lib

--- src/app/lib/bitcoin/bitcoin.utils.js
+++ src/app/lib/bitcoin/bitcoin.utils.js
@@ -1,3 +1,4 @@
+import CryptoJS from 'crypto-js'
 // Convert a byte array to a hex string
 const bytesToHex = bytes => {
   for (var hex = [], i = 0; i < bytes.length; i++) {
@@ -15,4 +16,30 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-export default { bytesToHex, getRandomInt }
+const encrypt = ({ message, password }) =>
+  new Promise((resolve, reject) => {
+    try {
+      const encrypted = CryptoJS.AES.encrypt(message, password).toString()
+
+      const decryptedMessage = CryptoJS.AES.decrypt(
+        encrypted,
+        password
+      ).toString(CryptoJS.enc.Utf8)
+      if (message !== decryptedMessage) reject(new Error('Encryption error'))
+
+      resolve(encrypted)
+    } catch (error) {
+      reject(error)
+    }
+  })
+
+const dencrypt = ({ encrypted, password }) =>
+  new Promise((resolve, reject) => {
+    try {
+      resolve(CryptoJS.AES.decrypt(encrypted, password))
+    } catch (error) {
+      reject(error)
+    }
+  })
+
+export default { bytesToHex, getRandomInt, encrypt, dencrypt }
