fc53d1135e3f8319080821591fcd43acbfba3804
Ernane Luis <ernane.luis@gmail.com>
2018-07-05 14:03:02 +0900

    refactor bitcoin lib mnemonic function

--- src/app/lib/bitcoin/bitcoin.mnemonic.js
+++ src/app/lib/bitcoin/bitcoin.mnemonic.js
@@ -3,7 +3,9 @@ import * as Mnemonic from 'bitcore-mnemonic'
 const createMnemonicFromSeed = seedHex =>
   new Promise((resolve, reject) => {
     try {
-      const seed = new Buffer(seedHex, 'hex')
+      let seed = new Buffer(seedHex, 'hex')
+      seed = seed.slice(0, 32)
+      // suppose to reproduce 24 words mnemonic
       const mnemonic = Mnemonic.fromSeed(seed, Mnemonic.Words.ENGLISH)
       resolve(mnemonic)
     } catch (error) {
@@ -11,10 +13,17 @@ const createMnemonicFromSeed = seedHex =>
     }
   })
 
-//import { HDPrivateKey } from 'bitcore-lib'
-// var pk = mnemonic.toHDPrivateKey('my password', 'testnet') // or livenet
-// console.log(pk)
+const createHDPrivateFromMnemonic = mnemonic =>
+  new Promise((resolve, reject) => {
+    try {
+      const mnemonicObj = new Mnemonic(mnemonic)
+      let hdPrivate = mnemonicObj.toHDPrivateKey('testnet') // or livenet
+      resolve(hdPrivate)
+    } catch (error) {
+      reject(error)
+    }
+  })
 
 // var mnemonic = new Mnemonic()
 // var seed = mnemonic.toSeed('my passphrase')
-export default { createMnemonicFromSeed }
+export default { createMnemonicFromSeed, createHDPrivateFromMnemonic }
