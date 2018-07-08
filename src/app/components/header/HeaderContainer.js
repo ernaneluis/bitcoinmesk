ec7f1c83a43438ff273b991dce934993b04d16bc
Ernane Luis <ernane.luis@gmail.com>
2018-07-08 19:53:12 +0900

    refactor header container to make create address a button to trigger create key action and not redirect to /create path ps: this is temporary, in future create address will be a page

--- src/app/components/header/HeaderContainer.js
+++ src/app/components/header/HeaderContainer.js
@@ -1,20 +1,36 @@
 import { connect } from 'react-redux'
-import {
-  toWallet,
-  toCreate,
-  toImport,
-} from '../../../store/actions/routerActions'
+import { toWallet, toImport } from '../../../store/actions/routerActions'
+
+import { createKey } from '../../../store/actions/walletActions'
 import Header from './Header'
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  masterPrivateKey: state.wallet.masterPrivateKey,
+  nounceDeriviation: state.wallet.vault.nounceDeriviation,
+})
 
 const mapDispatchToProps = dispatch => ({
+  dispatch,
   onToWallet: () => dispatch(toWallet()),
-  onToCreate: () => dispatch(toCreate()),
   onToImport: () => dispatch(toImport()),
+})
+
+const mergeProps = (
+  { masterPrivateKey, nounceDeriviation },
+  { dispatch, onToWallet, onToImport },
+  ...rest
+) => ({
+  masterPrivateKey,
+  nounceDeriviation,
+  dispatch,
+  onToWallet,
+  onToImport,
+  onToCreate: () =>
+    dispatch(createKey({ masterPrivateKey, nounceDeriviation })),
 })
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
+  mergeProps
 )(Header)
