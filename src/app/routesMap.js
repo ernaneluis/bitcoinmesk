b0abcd0c4c1550befa6f49f4169dc0fa1b968381
Ernane Luis <ernane.luis@gmail.com>
2018-07-08 19:49:24 +0900

    refactor routes maps to support lock scene and on wallet route to restore wallet vault, and fetch All keys before scene is rendered

--- src/app/routesMap.js
+++ src/app/routesMap.js
@@ -1,21 +1,41 @@
 import { redirect } from 'redux-first-router'
-import { toWallet, toInit } from '../store/actions/routerActions'
+import { toWallet, toInit, toLock } from '../store/actions/routerActions'
 import { isEmpty } from 'lodash'
+import { restoreWallet, fetchAllKeys } from '../store/actions/walletActions'
 
 const routesMap = {
   WALLET: {
     path: '/',
     thunk: (dispatch, getState) => {
-      if (isEmpty(getState().wallet.vault)) dispatch(redirect(toInit()))
+      // must call restore wallet on route because thunk is called first than persistWallet
+      dispatch(restoreWallet())
+
+      if (isEmpty(getState().wallet.vault.encryptedMnemonic))
+        dispatch(redirect(toInit()))
+      else if (isEmpty(getState().wallet.masterPrivateKey))
+        dispatch(redirect(toLock()))
+      else {
+        const keys = getState().wallet.keys
+        const masterPrivateKey = getState().wallet.masterPrivateKey
+        const nounceDeriviation = getState().wallet.vault.nounceDeriviation
+        if (keys.length !== nounceDeriviation)
+          dispatch(fetchAllKeys({ masterPrivateKey, nounceDeriviation }))
+      }
     },
   },
 
   INIT: {
     path: '/init',
     thunk: (dispatch, getState) => {
+      dispatch(restoreWallet())
       // TODO: more safe checks like this should be added in future
-      if (!isEmpty(getState().wallet.vault)) dispatch(redirect(toWallet()))
+      if (!isEmpty(getState().wallet.vault.encryptedMnemonic))
+        dispatch(redirect(toWallet()))
     },
+  },
+
+  LOCK: {
+    path: '/lock',
   },
 
   CREATE: {
