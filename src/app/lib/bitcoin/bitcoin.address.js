4274c493f6961680cbe0c4ebc6f6f0ab6e0af24f
Ernane Luis <ernane.luis@gmail.com>
2018-07-08 19:30:11 +0900

    refactor bitcoin address lib by removing not used code

--- src/app/lib/bitcoin/bitcoin.address.js
+++ src/app/lib/bitcoin/bitcoin.address.js
@@ -1,35 +1,3 @@
-import * as bitcoin from 'bitcoinjs-lib'
-import bip38 from 'bip38'
-import wif from 'wif'
+import { PrivateKey, Networks } from 'bitcore-lib'
 
-const createAddress = ({ passphrase }) =>
-  new Promise((resolve, reject) => {
-    try {
-      const pvKey = Buffer.from('correct horse battery staple')
-      const pvKeyWifEnconded = wif.encode(128, pvKey, true)
-      const pvKeyWifDecoded = wif.decode(pvKeyWifEnconded)
-
-      // encrypted key will be saved on local storage
-      const encryptedKey = bip38.encrypt(
-        pvKeyWifDecoded.privateKey,
-        pvKeyWifDecoded.compressed,
-        passphrase
-      )
-      resolve(encryptedKey)
-    } catch (error) {
-      reject(error)
-    }
-  })
-
-const openAddress = async ({ encryptedKey, passphrase }) => {
-  const decryptedKey = await bip38.decrypt(encryptedKey, passphrase)
-  const testpvKeyWifEnconded = wif.encode(
-    128,
-    decryptedKey.privateKey,
-    decryptedKey.compressed
-  )
-  console.log({ testpvKeyWifEnconded })
-  return testpvKeyWifEnconded
-}
-
-export default { createAddress, openAddress }
+export default {}
