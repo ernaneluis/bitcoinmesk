e028c775927119a961e914cab221572257b6dee0
Ernane Luis <ernane.luis@gmail.com>
2018-07-08 19:43:46 +0900

    refactor wallet reducer to support new wallet types, add new prop nounceDerivation to vault, rename privateKey to keys, add masterPrivateKey to init state

--- src/store/reducers/walletReducer.js
+++ src/store/reducers/walletReducer.js
@@ -1,32 +1,54 @@
 import {
-  CREATE_PRIVATE_KEY_FAILURE,
-  CREATE_PRIVATE_KEY_SUCCESS,
+  CREATE_KEY_FAILURE,
+  CREATE_KEY_SUCCESS,
   RESTORE_WALLET,
   SAVE_WALLET,
+  FETCH_MASTER_PRIVATE_KEY,
+  INCREASE_NOUNCE_DERIVATION,
+  FETCH_ALL_KEYS_SUCCESS,
 } from '../typesReducers'
 
-const initialState = {
+// not safe keep on local storage uncrypted data such as masterPrivateKey
+// only save encryptedMnemonic, passwordHint, nounce of Deriviation (from 0 to X) to be used on path derivation
+// once user add his password(lock scene) it will:
+// 1) dencrypted his mnemonic
+// 2) derive masterPrivateKey from mnmonic + password
+// 3) derive all private keys by for 0 to nounceDeriviation
+// ps: when create a new address then increment nounceDeriviation
+
+export const initialState = {
   vault: {
-    // HDPathderiver
     encryptedMnemonic: '',
+    nounceDeriviation: 0,
     passwordHint: '',
   },
-  privateKeys: [],
+  masterPrivateKey: '',
+  keys: [],
 }
 
 export default (state = initialState, { type, payload, error }) => {
   switch (type) {
-    case CREATE_PRIVATE_KEY_FAILURE:
+    case CREATE_KEY_FAILURE:
       return { ...state, error }
 
-    case CREATE_PRIVATE_KEY_SUCCESS:
-      console.log('CREATE_PRIVATE_KEY_SUCCESS', payload)
-      state.privateKeys.push(payload)
-      return { ...state, privateKeys: state.privateKeys }
+    case CREATE_KEY_SUCCESS:
+      console.log('CREATE_KEY_SUCCESS', payload)
+      return {
+        ...state,
+        // redux expect a new state object in order to call render
+        keys: [...state.keys, payload],
+      }
+
+    case FETCH_ALL_KEYS_SUCCESS:
+      console.log('FETCH_ALL_KEYS_SUCCESS', payload)
+      return {
+        ...state,
+        // redux expect a new state object in order to call render
+        keys: payload,
+      }
 
     case RESTORE_WALLET:
       console.log('RESTORE_WALLET', payload)
-
       return {
         ...state,
         vault: payload,
@@ -35,7 +57,23 @@ export default (state = initialState, { type, payload, error }) => {
     case SAVE_WALLET:
       return {
         ...state,
-        vault: payload,
+        vault: { ...payload, nounceDeriviation: 0 },
+      }
+
+    case FETCH_MASTER_PRIVATE_KEY:
+      console.log('FETCH_MASTER_PRIVATE_KEY', payload)
+      return {
+        ...state,
+        masterPrivateKey: payload,
+      }
+
+    case INCREASE_NOUNCE_DERIVATION:
+      console.log('INCREASE_NOUNCE_DERIVATION')
+      const nounceDeriviation = state.vault.nounceDeriviation + 1
+
+      return {
+        ...state,
+        vault: { ...state.vault, nounceDeriviation },
       }
 
     default:
