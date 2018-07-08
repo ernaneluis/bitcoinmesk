cc803c124210c384020d1f6ca66f5e73b551dd42
Ernane Luis <ernane.luis@gmail.com>
2018-07-08 19:54:46 +0900

    refactor wallet scene to show all address from keys state

--- src/app/scenes/wallet/Wallet.js
+++ src/app/scenes/wallet/Wallet.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Wallet = ({ mnemonic }) => (
+const Wallet = ({ keys }) => (
   <div>
-    <h2>{mnemonic}</h2>
+    {!!keys.length && (
+      <div>
+        {keys.map(({ privateKey, publicKey, address }, key) => (
+          <p key={key}> {address} </p>
+        ))}
+      </div>
+    )}
+
+    {!!!keys.length && <h2>no address create one now</h2>}
   </div>
 )
 
