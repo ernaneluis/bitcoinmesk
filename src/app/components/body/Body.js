16996400b0227a485ea3a21ffc880e5c47c79cc4
Ernane Luis <ernane.luis@gmail.com>
2018-07-10 02:18:28 +0900

    refactor body  to support new page names

--- src/app/components/body/Body.js
+++ src/app/components/body/Body.js
@@ -2,38 +2,28 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Wallet from '../../scenes/wallet/WalletContainer'
-import Init from '../../scenes/init/InitContainer'
-import Create from '../../scenes/create/CreateContainer'
-import Import from '../../scenes/import/ImportContainer'
+import New from '../../scenes/new/NewContainer'
+import Receive from '../../scenes/receive/ReceiveContainer'
+import Restore from '../../scenes/restore/RestoreContainer'
 import Lock from '../../scenes/lock/LockContainer'
+import Send from '../../scenes/send/SendContainer'
+import Welcome from '../../scenes/welcome/WelcomeContainer'
 
 const Container = styled.main.attrs({
   className: 'container',
 })`
   min-height: 500px;
-  &&& {
-    padding: 80px 15px 0;
-  }
-  @media (max-width: ${props => props.theme.screenSize.md}) {
-    &&& {
-      padding: 200px 15px 0;
-    }
-  }
-  @media (max-width: ${props => props.theme.screenSize.lg}) {
-    &.container {
-      width: 100%;
-      padding: 0px;
-    }
-  }
 `
 
 const Body = ({ scene }) => (
   <Container>
-    {scene === 'WALLET' && <Wallet />}
-    {scene === 'INIT' && <Init />}
+    {scene === 'NEW' && <New />}
     {scene === 'LOCK' && <Lock />}
-    {scene === 'CREATE' && <Create />}
-    {scene === 'IMPORT' && <Import />}
+    {scene === 'RESTORE' && <Restore />}
+    {scene === 'WALLET' && <Wallet />}
+    {scene === 'RECEIVE' && <Receive />}
+    {scene === 'SEND' && <Send />}
+    {scene === 'WELCOME' && <Welcome />}
   </Container>
 )
 
