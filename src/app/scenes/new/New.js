2de2e675f2a0a9f03870209a5b0cb155f9ca2bbf
Ernane Luis <ernane.luis@gmail.com>
2018-07-10 02:14:55 +0900

    rename init to new scene

--- src/app/scenes/new/New.js
+++ src/app/scenes/new/New.js
@@ -6,8 +6,11 @@ const Wrapper = styled.div`
   min-height: 500px;
 `
 
-const PreeSeed = styled.p`
+const PreeSeed = styled.div.attrs({
+  className: 'text-muted',
+})`
   word-break: break-all;
+  font-size: 0.7em;
 `
 
 const Point = styled.div.attrs({
@@ -23,26 +26,44 @@ const Point = styled.div.attrs({
   z-index: 999;
 `
 
-const Init = ({ onMouseMove, onSubmit, seed, points, mnemonic }) => (
+const New = ({ onMouseMove, onSubmit, seed, points, mnemonic }) => (
   <Wrapper onMouseMove={onMouseMove}>
+    <div className="row bg-dark text-white">
+      <div className="col-4">
+        <h3>Bitmask</h3>
+      </div>
+      <div className="col-8">
+        <small>Creating your new Wallet</small>
+      </div>
+    </div>
     {!mnemonic && (
-      <div>
-        <p>
-          Secure random number generation requires that you provide some
-          unpredictable data, also called "entropy". Please move your mouse
-          around.
-        </p>
-        <PreeSeed>{seed}</PreeSeed>
-        <small>{points.length}/200</small>
+      <div className="container">
+        <div className="row">
+          <p>
+            Secure random number generation requires that you provide some
+            unpredictable data, also called "entropy".
+          </p>
+          <p>Please move your mouse around.</p>
+          <div className="jumbotron">
+            <PreeSeed>{seed}</PreeSeed>
+          </div>
+        </div>
+        <div className="row text-center">
+          <small className="w-100">{points.length}/200</small>
+        </div>
         <div>
           {points.map(({ x, y }, key) => <Point key={key} x={x} y={y} />)}
         </div>
       </div>
     )}
-
     {mnemonic && (
       <form onSubmit={onSubmit}>
-        <h2>{mnemonic}</h2>
+        <p>This is your mnemonic recovery words:</p>
+        <div className="jumbotron">{mnemonic}</div>
+        <p>
+          <small>You must save this in order to backup your funds.</small>
+        </p>
+        <hr />
         <label className="col-form-label">Password</label>
         <Field
           className="form-control"
@@ -61,13 +82,13 @@ const Init = ({ onMouseMove, onSubmit, seed, points, mnemonic }) => (
         />
         <label className="col-form-label">Password Hint</label>
         <Field
-          className="form-control"
+          className="form-control mb-2"
           component="input"
           name="passwordHint"
           placeholder="Password Hint"
           type="text"
         />
-        <button type="submit" className="btn btn-success">
+        <button type="submit" className="btn btn-success btn-lg btn-block">
           Create Wallet
         </button>
       </form>
@@ -75,4 +96,4 @@ const Init = ({ onMouseMove, onSubmit, seed, points, mnemonic }) => (
   </Wrapper>
 )
 
-export default Init
+export default New
