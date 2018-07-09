07a5d3314add9acf1eceb2cb0543ce1fd2f41f4b
Ernane Luis <ernane.luis@gmail.com>
2018-07-10 02:23:06 +0900

    add abstract address methods based on provider request

--- src/app/lib/bitcoin/bitcoin.address.js
+++ src/app/lib/bitcoin/bitcoin.address.js
@@ -1,3 +1,13 @@
-import { PrivateKey, Networks } from 'bitcore-lib'
+import { Address, Networks } from 'bitcore-lib'
+import { getProvider } from './bitcoin.provider'
 
-export default {}
+export const getAddress = ({ address, provider = 'insight' }) =>
+  getProvider(provider).getAddress({ address })
+
+export const getBalance = ({ address, provider = 'insight' }) =>
+  getProvider(provider).getBalance({ address })
+
+export const getUtxo = ({ address, provider = 'insight' }) =>
+  getProvider(provider).getUtxo({ address })
+
+export default { getAddress, getUtxo, getBalance }
