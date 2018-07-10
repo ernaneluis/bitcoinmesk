6a6747f18e6aa590fb7fbe87a887d00eb38a7074
Ernane Luis <ernane.luis@gmail.com>
2018-07-11 00:33:49 +0900

    build receive container. it shows last key on vault

--- src/app/scenes/receive/ReceiveContainer.js
+++ src/app/scenes/receive/ReceiveContainer.js
@@ -1,27 +1,36 @@
 import { connect } from 'react-redux'
 import { reduxForm } from 'redux-form'
 import Receive from './Receive'
+import { isEmpty, last } from 'lodash'
+import { createKey } from '../../../store/actions/walletActions'
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  address: !isEmpty(state.wallet.vault.keys)
+    ? last(state.wallet.vault.keys).address
+    : '',
+  balance: 12.65426781,
+  masterPrivateKey: state.wallet.vault.masterPrivateKey,
+  nounceDeriviation: state.wallet.vault.nounceDeriviation,
+})
 
 const mapDispatchToProps = dispatch => ({
   dispatch,
 })
 
-const mergeProps = (stateProps, { dispatch }, { handleSubmit }) => ({
-  ...stateProps,
+const mergeProps = (
+  { address, balance, masterPrivateKey, nounceDeriviation },
+  { dispatch },
+  ...ownProps
+) => ({
+  address,
+  balance,
   dispatch,
-  onSubmit: handleSubmit(values => dispatch()),
+  onCreateNewAddress: () =>
+    dispatch(createKey({ masterPrivateKey, nounceDeriviation })),
 })
 
-export default reduxForm({
-  form: 'createForm',
-  destroyOnUnmount: false,
-  forceUnregisterOnUnmount: true,
-})(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps,
-    mergeProps
-  )(Receive)
-)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+)(Receive)
