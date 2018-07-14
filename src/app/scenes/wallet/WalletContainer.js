b8aba4fc27256a1e9ac5581ee7de8e6e40cc7390
Ernane Luis <ernane.luis@gmail.com>
2018-07-14 23:06:21 +0900

    refactor wallet container to support fetch all transactions from api provider

--- src/app/scenes/wallet/WalletContainer.js
+++ src/app/scenes/wallet/WalletContainer.js
@@ -1,53 +1,48 @@
 import { connect } from 'react-redux'
 import Wallet from './Wallet'
 import { fetchAllKeys } from '../../../store/actions/walletActions'
+import {
+  getLastAddress,
+  simplifyTransactions,
+  getBalance,
+} from '../../../store/selectors'
 
 import * as bitcoin from '../../lib/bitcoin'
 
-const mapStateToProps = state => ({
-  keys: state.wallet.vault.keys,
-  masterPrivateKey: state.wallet.masterPrivateKey,
-  nounceDeriviation: state.wallet.vault.nounceDeriviation,
-  transactions: [
-    {
-      date: Date(Date.now())
-        .toLocaleString()
-        .split('GMT')[0],
-      type: 'SEND',
-      amount: bitcoin.utils.satoshiToBitcoin(16231102),
-      address: '1Exi95PsBdV5PxrJGi7ZZ8Hk9uoLpgMVeV',
-      confirmations: 1,
-      txhash:
-        '3c4e8fb8c41cf029e89e302c45d6e6e3eeb89d34b6c985826303760a3308e75f',
-    },
-    {
-      date: Date(Date.now())
-        .toLocaleString()
-        .split('GMT')[0],
-      type: 'RECEIVE',
-      amount: bitcoin.utils.satoshiToBitcoin(16231102),
-      address: '1Exi95PsBdV5PxrJGi7ZZ8Hk9uoLpgMVeV',
-      confirmations: 1,
-      txhash:
-        '3c4e8fb8c41cf029e89e302c45d6e6e3eeb89d34b6c985826303760a3308e75f',
-    },
-  ],
-})
+const mapStateToProps = state => {
+  const address = getLastAddress(state)
+  const transactions = simplifyTransactions(
+    address,
+    state.wallet.vault.transactions[address]
+  )
+  const balance = getBalance(transactions)
+  return {
+    address,
+    balance,
+    transactions,
+    keys: state.wallet.vault.keys,
+    masterPrivateKey: state.wallet.masterPrivateKey,
+    nounceDeriviation: state.wallet.vault.nounceDeriviation,
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   dispatch,
 })
 
 const mergeProps = (
-  { keys, masterPrivateKey, nounceDeriviation, transactions },
+  { address, balance, keys, masterPrivateKey, nounceDeriviation, transactions },
   { dispatch },
   { handleSubmit }
 ) => ({
+  address,
+  balance,
   keys,
   masterPrivateKey,
   nounceDeriviation,
   transactions,
   dispatch,
+  // refreshBalance: () =>
 })
 
 export default connect(
