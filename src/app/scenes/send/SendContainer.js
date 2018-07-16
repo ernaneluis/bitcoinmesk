24cbefe609610828a8aab4cc3c3c0a2c67a9c9db
Ernane Luis <ernane.luis@gmail.com>
2018-07-17 00:44:15 +0900

    refactor send container to support new selectors and ask for password to confirm transaction

--- src/app/scenes/send/SendContainer.js
+++ src/app/scenes/send/SendContainer.js
@@ -1,23 +1,51 @@
 import { connect } from 'react-redux'
 import { reduxForm } from 'redux-form'
+import { redirect } from 'redux-first-router'
+import { isEmpty, uniq } from 'lodash'
+
 import Send from './Send'
 
-import { unlockWallet } from '../../../store/actions/walletActions'
+import * as bitcoin from '../../lib/bitcoin'
+import {
+  getAllUtxos,
+  getBalance,
+  getPrivateKeyFromAddresses,
+} from '../../../store/selectors'
+import { sendTransaction } from '../../../store/actions/sendActions'
+import { toLock } from '../../../store/actions/routerActions'
 
-const mapStateToProps = state => ({
-  fromAddress: state.location.payload.fromAddress,
-})
-
+const mapStateToProps = state => {
+  const addresses = uniq(getAllUtxos(state).map(utxo => utxo.address))
+  return {
+    balance: getBalance(state),
+    utxos: getAllUtxos(state),
+    privateKeys: getPrivateKeyFromAddresses({ state, addresses }),
+  }
+}
 const mapDispatchToProps = dispatch => ({
   dispatch,
 })
 
-const mergeProps = ({ fromAddress }, { dispatch }, { handleSubmit }) => ({
-  fromAddress,
+const mergeProps = (
+  { balance, utxos, privateKeys },
+  { dispatch },
+  { handleSubmit }
+) => ({
+  balance,
+  utxos,
+  privateKeys,
   dispatch,
   onSubmit: handleSubmit(({ toAddress, amount }) => {
-    console.log('submit')
-    // dispatch(unlockWallet({ toAddress, amount }))
+    isEmpty(privateKeys)
+      ? dispatch(redirect(toLock()))
+      : dispatch(
+          sendTransaction({
+            privateKeys,
+            toAddress,
+            amount,
+            utxos,
+          })
+        )
   }),
 })
 
